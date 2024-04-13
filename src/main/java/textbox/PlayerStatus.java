e681e4cce2ad24462a23452e00968f29a8a74915
B1G-SAM <54065426+B1G-SAM@users.noreply.github.com>
2024-04-14 01:55:44 +0800

    Handle exceptions for player status bar. Fix shop exiting commands.

--- src/main/java/textbox/PlayerStatus.java
+++ src/main/java/textbox/PlayerStatus.java
@@ -1,9 +1,12 @@
 package textbox;
 
-
 import map.PlayerInventory;
 
 public class PlayerStatus {
+    private static final int MAX_HEALTH = 100000;
+    private static final int MIN_VALUE = 0;
+    private static final int DEFAULT_HEALTH = 100;
+
     private int playerHealth;
     private int playerMoney;
     private int playerExp;
@@ -12,24 +15,33 @@ public class PlayerStatus {
     private int playerDamageAmp;
 
     public PlayerStatus(int startHealth, int startMoney, int startExp, int startDamage,
-                        PlayerInventory playerInventory){
-        this.playerHealth = startHealth;
-        this.playerMoney = startMoney;
-        this.playerExp = startExp;
-        this.playerDamage = startDamage;
+                        PlayerInventory playerInventory) {
+        this.playerHealth = setValidHealth(startHealth);
+        this.playerMoney = Math.max(startMoney, MIN_VALUE);
+        this.playerExp = Math.max(startExp, MIN_VALUE);
+        this.playerDamage = Math.max(startDamage, MIN_VALUE);
         this.playerInventory = playerInventory;
         this.playerDamageAmp = 0;
     }
 
-    public int getPlayerHealth(){
+    private int setValidHealth(int health) {
+        if (health <= 1 || health > MAX_HEALTH) {
+            System.out.println("INVALID HEALTH DETECTED. RESETTING TO DEFAULT");
+            return DEFAULT_HEALTH;
+
+        }
+        return health;
+    }
+
+    public int getPlayerHealth() {
         return this.playerHealth;
     }
 
-    public int getPlayerMoney(){
+    public int getPlayerMoney() {
         return this.playerMoney;
     }
 
-    public int getPlayerExp(){
+    public int getPlayerExp() {
         return this.playerExp;
     }
 
@@ -53,16 +65,27 @@ public class PlayerStatus {
         this.playerDamage = playerDamage;
     }
 
-    public void harmHealth(int dmg){
-        playerHealth -= dmg;
+    public void harmHealth(int dmg) {
+        int newHealth = this.playerHealth - dmg;
+        setPlayerHealth(newHealth);
     }
 
-    public void addMoney(int money){
-        playerMoney += money;
+    public void addMoney(int money) {
+        long newMoney = (long) this.playerMoney + money;
+        if (newMoney > Integer.MAX_VALUE) {
+            this.playerMoney = Integer.MAX_VALUE;
+        } else {
+            this.playerMoney = (int) newMoney;
+        }
     }
 
-    public void addExp(int exp){
-        playerExp += exp;
+    public void addExp(int exp) {
+        long newExp = (long) this.playerExp + exp;
+        if (newExp > Integer.MAX_VALUE) {
+            this.playerExp = Integer.MAX_VALUE;
+        } else {
+            this.playerExp = (int) newExp;
+        }
     }
 
     public PlayerInventory getPlayerInventory() {
@@ -70,14 +93,15 @@ public class PlayerStatus {
     }
 
     public void setPlayerInventory(PlayerInventory playerInventory) {
+        if (playerInventory == null) throw new NullPointerException("Player inventory cannot be null.");
         this.playerInventory = playerInventory;
     }
 
-    public int getPlayerDamageAmp(){
+    public int getPlayerDamageAmp() {
         return playerDamageAmp;
     }
 
-    public void setPlayerDamageAmp(int dmg){
-        playerDamageAmp = dmg;
+    public void setPlayerDamageAmp(int dmg) {
+        this.playerDamageAmp = Math.max(dmg, MIN_VALUE);
     }
 }
